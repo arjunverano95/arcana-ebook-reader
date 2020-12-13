0a3c0800f6900d01a2f1d8d91a1526c3cf38c854
Arjun <i.ekzeed.av@gmail.com>
2020-12-13 14:56:19 +0800

    fix storage permission request

---
+++ android/app/src/main/kotlin/com/silversoft/arcana_ebook_reader/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.silversoft.arcana_ebook_reader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/widgets/importBooks.dart
+++ lib/widgets/importBooks.dart
@@ -4,37 +4,40 @@ import 'dart:typed_data';
 import 'package:arcana_ebook_reader/env.dart';
 import 'package:arcana_ebook_reader/util/context.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 // import 'package:isolate_handler/isolate_handler.dart';
 import 'package:uuid/uuid.dart';
 import 'package:epub/epub.dart' as Epub;
 import 'package:image/image.dart' as ImageObj;
 
-void openImportDialog() {
-  // final isolates = IsolateHandler();
-  FilePicker.platform.pickFiles(
-    allowMultiple: false,
-    type: FileType.custom,
-    allowedExtensions: ['epub'],
-  ).then((result) async {
-    if (result != null) {
-      if (result.files.length > 0) {
-        // SINGLE FILE ONLY
+Future<void> openImportDialog() async {
+  if (await Permission.storage.request().isGranted) {
+    // final isolates = IsolateHandler();
+    FilePicker.platform.pickFiles(
+      allowMultiple: false,
+      type: FileType.custom,
+      allowedExtensions: ['epub'],
+    ).then((result) async {
+      if (result != null) {
+        if (result.files.length > 0) {
+          // SINGLE FILE ONLY
 
-        // String file = result.files[0].path  + "|"+  result.files[0].extension.toLowerCase();
-        // isolates.spawn<bool>(importBooks,
-        //     name: 'importBooks',
-        //     onReceive: (value) {
-        //       isolates.kill('importBooks');
-        //     },
-        //     onInitialized: () =>
-        //         isolates.send(file, to: 'importBooks'));
+          // String file = result.files[0].path  + "|"+  result.files[0].extension.toLowerCase();
+          // isolates.spawn<bool>(importBooks,
+          //     name: 'importBooks',
+          //     onReceive: (value) {
+          //       isolates.kill('importBooks');
+          //     },
+          //     onInitialized: () =>
+          //         isolates.send(file, to: 'importBooks'));
 
-        _importBooks(
-                result.files[0].path, result.files[0].extension.toLowerCase())
-            .then((value) => env.bookstore.getBooks());
+          _importBooks(
+                  result.files[0].path, result.files[0].extension.toLowerCase())
+              .then((value) => env.bookstore.getBooks());
+        }
       }
-    }
-  });
+    });
+  }
 }
 
 // This function happens in the isolate.

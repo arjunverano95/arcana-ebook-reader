6477e14ff3d7ab200e15dcd952809ccb7632df02
Arjun <i.ekzeed.av@gmail.com>
2020-12-13 15:03:41 +0800

    added splash screen

--- android/app/src/main/kotlin/com/silversoft/arcana_ebook_reader/MainActivity.kt
+++ android/app/src/main/kotlin/com/silversoft/arcana_ebook_reader/MainActivity.kt
@@ -2,5 +2,8 @@ package com.silversoft.arcana_ebook_reader
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }

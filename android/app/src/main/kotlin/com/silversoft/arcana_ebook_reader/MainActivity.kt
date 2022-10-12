ae19a4e8610e96d94d955f15a74387373fef12d1
Arjun Verano <72175043+arjunverano95@users.noreply.github.com>
2022-10-12 17:02:16 +0800

    updated android and ios

--- android/app/src/main/kotlin/com/silversoft/arcana_ebook_reader/MainActivity.kt
+++ android/app/src/main/kotlin/com/silversoft/arcana_ebook_reader/MainActivity.kt
@@ -2,8 +2,5 @@ package com.silversoft.arcana_ebook_reader
 
 import io.flutter.embedding.android.FlutterActivity
 
-import android.os.Build
-import android.view.ViewTreeObserver
-import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
